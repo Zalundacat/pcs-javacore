2b3f6ea1dff513a29ccab336633997f1253eb5dc
Zalundacat <101882998+Zalundacat@users.noreply.github.com>
2022-07-04 15:10:10 +0300

    Add files via upload

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -5,17 +5,55 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    static class Requests {
+        String type, task;
+
+        public Requests(String type, String task) {
+            this.type = type;
+            this.task = task;
+        }
+
+        @Override
+        public String toString() {
+            return "type = '" + type + "', task = '" + task + "'";
+        }
+    }
+
+    private final int port;
+    private final Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
-        System.out.println("Starting server at " + port + "...");
-        //...
+        try (ServerSocket serverSocket = new ServerSocket(this.port)) {
+            System.out.println("Starting server at " + this.port + "...");
+            while (true) {
+                try (
+                        Socket clientSocket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+                ) {
+                    String json = in.readLine();
+                    Requests request = new Gson().fromJson(json, Requests.class);
+                    switch (request.type) {
+                        case "ADD":
+                            todos.addTask(request.task);
+                            break;
+                        case "REMOVE":
+                            todos.removeTask(request.task);
+                            break;
+                    }
+                    out.println(todos.getAllTasks());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
