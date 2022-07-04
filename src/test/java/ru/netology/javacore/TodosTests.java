e381ef9cfa4a7eb1c06ef1dd088b92693aa61f76
Zalundacat <101882998+Zalundacat@users.noreply.github.com>
2022-07-04 14:23:08 +0300

    Add files via upload

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,49 @@
 package ru.netology.javacore;
 
+import org.junit.jupiter.api.*;
+
 public class TodosTests {
 
-    // ваши тесты для класса Todos
+    @Test
+    public void addTaskTest() {
+
+        Todos todo = new Todos();
+
+        todo.addTask("Задача 1");
+
+        boolean actual = todo.getAllTasks().contains("Задача 1");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void removeTaskTest() {
+
+        Todos todo = new Todos();
+
+        todo.addTask("Очередное задание");
+        todo.removeTask("Очередное задание");
+
+        boolean actual = todo.getAllTasks().contains("Очередное задание");
+
+        Assertions.assertFalse(actual);
+    }
+
+
+    @Test
+    public void getAllTasksTest() {
+
+        Todos todo = new Todos();
+
+        String expected = "Задание 1" + " "
+                + "Задание 2" + " "
+                + "Задание 3" + " ";
+
+        todo.addTask("Задание 1");
+        todo.addTask("Задание 2");
+        todo.addTask("Задание 3");
+
+        Assertions.assertEquals(expected, todo.getAllTasks());
+    }
+
 }
